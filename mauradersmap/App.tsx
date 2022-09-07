954559d00d59f293690b09008ce8a3cbd205c0bf
ferejbo <felix.rej.bottolfsen@gmail.com>
2022-09-07 14:55:55 +0200

    Added little test component

--- mauradersmap/App.tsx
+++ mauradersmap/App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import Test from "./src/components/test/Test";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to mauraders map!!</Text>
+      <Test />
       <StatusBar style="auto" />
     </View>
   );
