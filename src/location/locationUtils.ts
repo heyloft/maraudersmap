b8faa150eecd8c88cc10b8ceb5fe24ab4e2b5a1f
jonasoo98 <jonasoo@stud.ntnu.no>
2022-09-23 14:23:44 +0200

    Change parameter type of distance

--- src/location/locationUtils.ts
+++ src/location/locationUtils.ts
@@ -1,14 +1,18 @@
+import { LocationObjectCoords } from "expo-location";
 /**
  * Calculates the distance in metres between 2 locations using the haversine formula.
  * @param loc1 - The firs location. Tuple with [lat, long] coordinates.
  * @param loc2 - The second location. Tuple with [lat, long] coordinates.
  * @returns - The distance in meters between loc1 and loc2.
  */
-export const distance = (loc1: number[], loc2: number[]) => {
-  const lat1 = (loc1[0] * Math.PI) / 180; // Convert to radians.
-  const long1 = (loc1[1] * Math.PI) / 180;
-  const lat2 = (loc2[0] * Math.PI) / 180;
-  const long2 = (loc2[1] * Math.PI) / 180;
+export const distance = (
+  loc1: LocationObjectCoords,
+  loc2: LocationObjectCoords
+) => {
+  const lat1 = (loc1.latitude * Math.PI) / 180; // Convert to radians.
+  const long1 = (loc1.longitude * Math.PI) / 180;
+  const lat2 = (loc2.latitude * Math.PI) / 180;
+  const long2 = (loc2.longitude * Math.PI) / 180;
   const lat_diff = lat1 - lat2;
   const long_diff = long1 - long2;
   // Calculate haversine:
