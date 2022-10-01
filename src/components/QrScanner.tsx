d2bdc3655fb9e663d270ad92aade1a79addd3219
Mathias Oterhals Myklebust <mathias@oterbust.no>
2022-10-01 11:59:37 +0200

    Add QrScanner to scanner tab

--- src/components/QrScanner.tsx
+++ src/components/QrScanner.tsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  ActivityIndicator,
+} from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 
 export default function QrScanner() {
@@ -26,21 +32,28 @@ export default function QrScanner() {
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   };
 
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
-
   return (
     <View style={styles.container}>
-      <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
-      />
-      {scanned && (
-        <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
+      {hasPermission === null ? (
+        <>
+          <ActivityIndicator size={"large"} color="#1E88E5" />
+          <Text style={{ marginTop: 20 }}>Requesting camera permission</Text>
+        </>
+      ) : !hasPermission ? (
+        <Text>No access to camera</Text>
+      ) : (
+        <>
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+          />
+          {scanned && (
+            <Button
+              title={"Tap to Scan Again"}
+              onPress={() => setScanned(false)}
+            />
+          )}
+        </>
       )}
     </View>
   );
@@ -49,7 +62,6 @@ export default function QrScanner() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
