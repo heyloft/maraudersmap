cbda5b57f85be76fd4e8fbda073ecdf613544f53
jonasoo98 <jonasoo@stud.ntnu.no>
2022-10-11 21:54:29 +0200

    Improve readablilty

--- src/components/QuestCompletedModal.tsx
+++ src/components/QuestCompletedModal.tsx
@@ -1,70 +1,61 @@
-import * as React from "react";
+import { useState } from "react";
 import { View, StyleSheet } from "react-native";
-import { Modal, Portal, Text, Button, Provider } from "react-native-paper";
+import { Modal, Portal, Text, Button } from "react-native-paper";
 import ConfettiCannon from "react-native-confetti-cannon";
 
 const QuestCompletedModal = () => {
-  const [visible, setVisible] = React.useState(false);
-
-  const showModal = () => setVisible(true);
-  const hideModal = () => setVisible(false);
+  const [visible, setVisible] = useState(false);
 
   return (
-    <>
-      <Provider>
-        <Portal>
-          <Modal
-            visible={visible}
-            onDismiss={hideModal}
-            contentContainerStyle={styles.container}
-          >
-            <ConfettiCannon
-              count={50}
-              fadeOut={true}
-              explosionSpeed={1000}
-              fallSpeed={3500}
-              origin={{ x: -0, y: 0 }}
-            ></ConfettiCannon>
-            <View style={{ justifyContent: "flex-start" }}>
-              <Text
-                style={{ fontSize: 30, textAlign: "center", paddingTop: 30 }}
-              >
-                Quest Completed!
-              </Text>
-            </View>
-            <View style={{ flex: 1, justifyContent: "center" }}>
-              <Text style={{ fontSize: 20, textAlign: "center" }}>
-                Congratulations you completed Gathering of Easter Eggs!
-                🥳🥳🥳🥳🥳🥳🥳
-              </Text>
-              <Button
-                mode="contained"
-                onPress={hideModal}
-                style={{ marginTop: 50 }}
-                color="green"
-              >
-                Continue
-              </Button>
-            </View>
-          </Modal>
-        </Portal>
-        <Button style={{ marginTop: 30 }} onPress={showModal}>
-          Show
-        </Button>
-      </Provider>
-    </>
+    <View>
+      <Portal>
+        <Modal
+          visible={visible}
+          onDismiss={() => setVisible(false)}
+          contentContainerStyle={styles.modalContainer}
+        >
+          <ConfettiCannon
+            count={50}
+            fadeOut={true}
+            explosionSpeed={1500}
+            fallSpeed={2000}
+            origin={{ x: 0, y: 0 }}
+          />
+          <View style={styles.container}>
+            <Text style={{ fontSize: 30 }}>Quest Completed!</Text>
+            <Text style={{ fontSize: 20 }}>
+              Congratulations you completed Gathering of Easter Eggs!
+            </Text>
+            <Button
+              mode="contained"
+              onPress={() => setVisible(false)}
+              color="green"
+            >
+              Continue
+            </Button>
+          </View>
+        </Modal>
+      </Portal>
+      <Button style={{ marginTop: 30 }} onPress={() => setVisible(true)}>
+        Show
+      </Button>
+    </View>
   );
 };
 
 export default QuestCompletedModal;
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "rgba(255, 255, 255, 0.9)",
-    margin: 30,
-    justifyContent: "flex-start",
-    alignItems: "center",
+  modalContainer: {
+    backgroundColor: "(255, 255, 255, 0.95)",
     flex: 1,
+    margin: 30,
     borderRadius: 10,
+  },
+  container: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "space-around",
+    alignItems: "center",
   },
 });
