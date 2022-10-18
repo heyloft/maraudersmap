a708a5df29798badb54761b7287b629471856f0b
Mathias Oterhals Myklebust <mathias@oterbust.no>
2022-10-18 18:56:29 +0200

    Remove debugging snippet

--- App.tsx
+++ App.tsx
@@ -5,15 +5,12 @@ import { RecoilRoot } from "recoil";
 import { notificationSetup } from "./src/notifications/notifications";
 import { QueryClient, QueryClientProvider } from "react-query";
 import AppNavigator from "./src/screens/AppNavigator";
-import createUser from "./src/api/utils/createUser";
 
 const queryClient = new QueryClient();
 
 export default function App() {
   useEffect(() => {
     notificationSetup();
-    console.log("create user");
-    createUser("snegle_Marit").then((res) => console.log(res));
   }, []);
 
   return (
