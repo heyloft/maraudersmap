04a90b1c0b3018768fd0d538d93e6a6cedc82b56
victoriabi <victorbi@stud.ntnu.no>
2022-10-25 13:14:10 +0200

    dismiss modal when clicking on button or outside modal

--- src/components/WelcomeModal.tsx
+++ src/components/WelcomeModal.tsx
@@ -2,24 +2,25 @@ import { Modal, Text, Button, Portal } from "react-native-paper";
 import { StyleSheet, View } from "react-native";
 import { useRecoilValue } from "recoil";
 import { currentUser } from "../recoil/atom";
+import React, { useState } from "react";
 
-const WelcomeModal = ({ onDismiss }: { onDismiss: () => void }) => {
+const WelcomeModal = () => {
   const user = useRecoilValue(currentUser);
+  const [isShown, setShown] = useState<boolean>(false);
+
   return (
     <Portal>
       <Modal
-        visible={user !== null}
-        onDismiss={onDismiss}
+        visible={user != null && isShown == false}
+        onDismiss={() => setShown(true)}
         contentContainerStyle={styles.modalContainer}
       >
         <View style={styles.container}>
           <Text style={{ fontSize: 27, fontWeight: "500" }}>Welcome!</Text>
-
           <Text style={{ fontSize: 20, textAlign: "center", margin: 15 }}>
             Introduction text...
           </Text>
-
-          <Button mode="contained" onPress={onDismiss} color="green">
+          <Button mode="contained" onPress={() => setShown(true)} color="green">
             Get started
           </Button>
         </View>
